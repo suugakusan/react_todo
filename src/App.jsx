093fba4924bd3c7380207841915a466a85e58c9f
suugakusan <87798070+suugakusan@users.noreply.github.com>
2021-09-19 16:29:26 +0900

    css適用

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,33 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="Todoを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTodo</p>
+      <div className="imcomp">
+        <p className="title">未完了のTodo</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>テスト</li>
             <button>完了</button>
             <button>削除</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>テスト</li>
             <button>完了</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
-        <p>完了のTodo</p>
+      <div className="comp">
+        <p className="title">完了のTodo</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>テスト</li>
             <button>戻す</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>テスト</li>
             <button>戻す</button>
           </div>
