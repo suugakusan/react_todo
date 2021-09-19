58c22d7cec96c2af161254750652ac4a3ac32e69
suugakusan <87798070+suugakusan@users.noreply.github.com>
2021-09-19 18:12:59 +0900

    完了機能の実装

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,14 @@ export const App = () => {
     newTodos.splice(index, 1);
     setInCompleteTodos(newTodos);
   };
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setInCompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
   return (
     <>
       <div className="input-area">
@@ -37,7 +45,7 @@ export const App = () => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
