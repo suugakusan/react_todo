d3f6f631f8259314b0b67639e4ae0d6e7f47c1d9
suugakusan <87798070+suugakusan@users.noreply.github.com>
2021-09-19 16:05:49 +0900

    事前準備

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,40 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="Todoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTodo</p>
+        <ul>
+          <div>
+            <li>テスト</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>テスト</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTodo</p>
+        <ul>
+          <div>
+            <li>テスト</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>テスト</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
